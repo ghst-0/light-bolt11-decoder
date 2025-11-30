d80d86757749c2fde6c435209a073088343f4dc7
ghost <ghst-0@users.noreply.github.com>
2025-12-01 06:34:48 +0700

    Remove buffer polyfill

--- examples/build.js
+++ examples/build.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 import esbuild from 'esbuild'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 await esbuild
   .build({
     entryPoints: ['demo.jsx'],
     outfile: 'demo.build.js',
     bundle: true,
-    plugins: [NodeGlobalsPolyfillPlugin({buffer: true})],
     define: {
       window: 'self',
       global: 'self'
